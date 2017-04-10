cfbc95b0da9018d5c61fdcc8bdc1813f5f35455c
M. Sonntag <michael.p.sonntag@gmail.com>
2017-04-10 15:47:03 +0200

    [src/test] update to es lint js file style

--- src/calc.test.js
+++ src/calc.test.js
@@ -2,15 +2,15 @@
  * Unit tests for src/calc.js
  */
 
-import { before, after, describe, it } from 'mocha'
-import { assert } from 'chai'
+import { describe, it } from "mocha"
+import { assert } from "chai"
 
-import { sumReturn } from './calc.js'
+import { sumReturn } from "./calc.js"
 
-describe('Testing simple function', () => {
-    it('test simple math', () => {
-      assert.isOk(2 === sumReturn(1,1), 'This should always pass')
-      assert.isNotOk(2 === sumReturn(1,2), 'This should always fail')
+describe("Testing simple function", () => {
+    it("test simple math", () => {
+        assert.isOk(2 === sumReturn(1,1), "This should always pass")
+        assert.isNotOk(2 === sumReturn(1,2), "This should always fail")
     })
 })
 
