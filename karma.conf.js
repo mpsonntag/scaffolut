c8660ebfb8f046469ca93a436b5c282d3867a1e6
M. Sonntag <michael.p.sonntag@gmail.com>
2017-04-10 19:22:28 +0200

    [travis] add chrome for travis

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
 
     let testBrowsers = ["Opera", "Chromium", "Firefox"]
     if (process.env.TRAVIS) {
-        testBrowsers = ["PhantomJS", "Firefox"]
+        testBrowsers = ["PhantomJS", "Firefox", "Chrome"]
     }
 
     config.set({
